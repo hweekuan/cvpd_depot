aa6d00dab82051855ed2fe093f5033e74f503f71
hweekuan <hweekuan@yahoo.com>
2021-08-25 08:33:01 +0800

    Add files via upload

---
+++ system_logs.py
@@ -0,0 +1,58 @@
+# copyright: code created by Lee Hwee Kuan 25August2021
+# 
+# this is a singleton class for printing system logs, pid, machine name, memory usage
+# device name etc so that you can trace where, when, what resources are being used
+# when you run your code. for a demo on how to use this code, look at system_logs_demo.py
+
+
+import os
+import platform
+import numpy as np
+import psutil
+from datetime import datetime
+
+class system_logs(object):
+
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not system_logs.__instance:
+            system_logs.__instance = object.__new__(cls)
+        return system_logs.__instance
+
+    def __init__(self,mydevice):
+        system_logs.__instance.pid = os.getpid()
+        system_logs.__instance.uname = os.uname()
+        system_logs.__instance.mydevice = mydevice
+        system_logs.__instance.start_time = datetime.now().strftime("%Y%m%d, %H:%M:%S")
+        system_logs.__instance.memory_usage = []
+
+    @staticmethod
+    def print_start_logs():
+        print('pid : ', system_logs.__instance.pid)
+        print('uname : ', system_logs.__instance.uname)
+        print('code run start time ',system_logs.__instance.start_time)
+        system_logs.__instance.mydevice.device_name()
+        print('memory use list in print start ',system_logs.__instance.memory_usage)
+
+    @staticmethod
+    def record_memory_usage(t):
+        mem_use = psutil.virtual_memory()[2]
+        print('mem use list ', system_logs.__instance.memory_usage)
+        system_logs.__instance.memory_usage.append(mem_use)
+        print('mem use list ', system_logs.__instance.memory_usage)
+        print('memory usage :',mem_use,' at t=',t)
+
+    @staticmethod
+    def print_end_logs():
+        now = datetime.now()
+        dt_string = now.strftime("%Y%m%d, %H:%M:%S")
+
+        print('end date/time every checkpoint:', dt_string)
+        # run_duration = ...
+
+        mean_memory = np.mean(system_logs.__instance.memory_usage)
+        std_memory = np.std(system_logs.__instance.memory_usage)
+        print('mean mem : ', mean_memory, ', std mem : ', std_memory )
+
+
