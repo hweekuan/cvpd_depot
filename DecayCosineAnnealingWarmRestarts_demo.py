f0531c5a852cd5d5b6738fe69a24ba4b2f592945
hweekuan <hweekuan@yahoo.com>
2021-12-31 15:57:42 +0800

    Add files via upload

---
+++ DecayCosineAnnealingWarmRestarts_demo.py
@@ -0,0 +1,38 @@
+# written by Lee Hwee Kuan, 31Dec2021
+#
+# the problem with cosine annealing is that the lr oscillates and 
+# at larger lr, it can throw off the optimiser. usually cosine lr 
+# scheduler is use for small enough max lr. added a functionality 
+# that have a decay max lr but operate like a cosine annealing lr scheme.
+
+import torch
+import torch.optim as optim
+import torch.nn    as nn
+
+from DecayCosineAnnealingWarmRestarts import DecayCosineAnnealingWarmRestarts
+
+class model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc = nn.Linear(1,1)
+    def forward(self,x):
+        return self.fc(x)
+
+if __name__=='__main__':
+
+    net = model()
+    lr = 1e-2
+    step = 50
+    decay = 0.9
+
+    opt = optim.Adam(net.parameters(),lr)
+    #print('opt ',opt)
+    sch = DecayCosineAnnealingWarmRestarts(opt,step,decay)   
+
+    for e in range(200):
+        print(e,' ',opt.param_groups[0]['lr'])
+        sch.step()
+
+
+
+
