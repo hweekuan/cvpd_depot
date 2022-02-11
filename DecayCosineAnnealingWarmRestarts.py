fead2a2d1a0c1d57a14571279da53aa89f01c22b
hweekuan <hweekuan@yahoo.com>
2022-02-11 12:04:57 +0800

    Add files via upload

--- DecayCosineAnnealingWarmRestarts.py
+++ DecayCosineAnnealingWarmRestarts.py
@@ -1,11 +1,12 @@
 # written by Lee Hwee Kuan, 31Dec2021
+# written by Lee Hwee Kuan, 11Feb2022
 #
 # the problem with cosine annealing is that the lr oscillates and 
 # at larger lr, it can throw off the optimiser. usually cosine lr 
 # scheduler is use for small enough max lr. added a functionality 
 # that have a decay max lr but operate like a cosine annealing lr scheme.
 
-
+import torch
 import torch.optim as optim
 
 class DecayCosineAnnealingWarmRestarts:
@@ -19,6 +20,8 @@ class DecayCosineAnnealingWarmRestarts:
         self.decay = decay
         self.cntr = 0
 
+        print('state dict ',self.state_dict())
+
 
     def step(self):
 
@@ -31,6 +34,47 @@ class DecayCosineAnnealingWarmRestarts:
             self.thrsh = self.thrsh*self.decay
 
         self.opt.param_groups[0]['lr'] = min(cur_lr,self.thrsh)
+
+
+    def state_dict(self):
+        return { "cos_dict" : self.cosine_sch.state_dict(), "thrsh" : self.thrsh, "cntr" : self.cntr }
+
+
+    def load_state_dict(self,read_dict):
+        self.cosine_sch.load_state_dict(read_dict['cos_dict'])
+        self.thrsh = read_dict['thrsh']
+        self.cntr = read_dict['cntr']
+
+
+
+
+# for testing code
+if __name__=='__main__':
+
+
+    param = torch.rand([2],requires_grad=True)
+    lr = 1
+    opt = optim.SGD([param],lr)
+
+    dca = DecayCosineAnnealingWarmRestarts(opt,10,.5)
+
+    dca.step() # perform 3 steps and see how
+    dca.step()
+    dca.step()
+
+    print('state dict before ',dca.state_dict())
+    print('cos dict before ',dca.state_dict()['cos_dict'])
+
+    torch.save({'sch': dca.state_dict()},'testfile.pth')
+
+    ckpt = torch.load('testfile.pth')
+
+    dca.load_state_dict(ckpt['sch'])
+
+    print('========')
+    print('state dict ',dca.state_dict())
+    print('cos dict after ',dca.state_dict()['cos_dict'])
+
 
 
 
