8dd4e360690734580340f6cd7381bbf27183786d
hweekuan <hweekuan@yahoo.com>
2024-10-24 14:44:42 +0800

    Add files via upload

--- system_logs.py
+++ system_logs.py
@@ -5,10 +5,12 @@
 # when you run your code. for a demo on how to use this code, look at system_logs_demo.py
 
 
+import torch
 import os
 import platform
 import numpy as np
 import psutil
+import time
 from datetime import datetime
 
 class system_logs(object):
@@ -20,13 +22,31 @@ class system_logs(object):
             system_logs.__instance = object.__new__(cls)
         return system_logs.__instance
 
-    def __init__(self,mydevice):
+    def __init__(self,mydevice,snooze_time):
         system_logs.__instance.pid = os.getpid()
         system_logs.__instance.uname = os.uname()
         system_logs.__instance.mydevice = mydevice
         system_logs.__instance.start_time = datetime.now() #.strftime("%Y%m%d, %H:%M:%S")
-        system_logs.__instance.memory_usage = []
+        system_logs.__instance.start_time_sec = time.time() #.strftime("%Y%m%d, %H:%M:%S")
+        system_logs.__instance.last_snooze_time = system_logs.__instance.start_time_sec 
+        system_logs.__instance.snooze_time = snooze_time   # in seconds
+        system_logs.__instance.cpu_memory_usage = []
+        system_logs.__instance.cuda_memory_usage = []
+        system_logs.__instance.start_cuda_memory = system_logs.__instance.current_cuda_memory()
 
+    # record total memory use in current cuda device
+    @staticmethod
+    def current_cuda_memory():
+        # print the cuda memory usage
+        #free, total = torch.cuda.mem_get_info(system_logs.__instance.mydevice.get())
+        free, total = torch.cuda.mem_get_info()
+        total = total / 1024**2
+        free  = free  / 1024**2
+        mem_used_MB = (total - free)
+        return mem_used_MB
+
+    # print the pid,machine name,run start time
+    # pid can be use to kill the job 
     @staticmethod
     def print_start_logs():
         print('pid : ', system_logs.__instance.pid)
@@ -37,19 +57,42 @@ class system_logs(object):
     @staticmethod
     def record_memory_usage(t):
         mem_use = psutil.virtual_memory()[2]
-        system_logs.__instance.memory_usage.append(mem_use)
-        print('memory usage :',mem_use,' at t=',t)
+        cur_mem =  system_logs.__instance.current_cuda_memory()
+        cuda_mem = cur_mem-system_logs.__instance.start_cuda_memory
+        system_logs.__instance.cpu_memory_usage.append(mem_use)
+        system_logs.__instance.cuda_memory_usage.append(cuda_mem)
+        print('cpu  memory usage :',mem_use,' at t=',t)
+        print('cuda memory usage :',cuda_mem,' at t=',t)
+
+
+    # check that if time has passed since last snooze time
+    # this can be use to decide on code checkpoint, e.g. save model etc
+    @staticmethod
+    def check_alarm():
+        now = time.time()
+        run_duration = now - system_logs.__instance.last_snooze_time
+        since_start = now - system_logs.__instance.start_time_sec
+        if run_duration>system_logs.__instance.snooze_time:
+            system_logs.__instance.last_snooze_time = now
+            print('reset snooze time to be ',since_start)
+            return True
+        return False
+
 
     @staticmethod
-    def print_end_logs():
+    def print_current_logs():
         now = datetime.now()
 
-        print('end date/time every checkpoint:', now.strftime("%Y%m%d, %H:%M:%S"))
+        print('current date/time:', now.strftime("%Y%m%d, %H:%M:%S"))
         run_duration = now - system_logs.__instance.start_time
-        print('run time ',run_duration)
+        print('run duration ',run_duration)
 
-        mean_memory = np.mean(system_logs.__instance.memory_usage)
-        std_memory = np.std(system_logs.__instance.memory_usage)
-        print('mean mem : ', mean_memory, ', std mem : ', std_memory )
+        mean_cpu_memory  = np.mean(system_logs.__instance.cpu_memory_usage)
+        std_cpu_memory   = np.std(system_logs.__instance.cpu_memory_usage)
+        mean_cuda_memory = np.mean(system_logs.__instance.cuda_memory_usage)
+        std_cuda_memory  = np.std(system_logs.__instance.cuda_memory_usage)
+
+        print('cpu  mean mem : ', mean_cpu_memory,  ', std mem : ', std_cpu_memory )
+        print('cuda mean mem : ', mean_cuda_memory, ', std mem : ', std_cuda_memory )
 
 
--- system_logs_demo.py
+++ system_logs_demo.py
@@ -1,14 +1,9 @@
 from system_logs import system_logs
+from mydevice    import mydevice
 
 import time
 import torch
 
-# =================================
-class device:
-    def __init__(self):
-        pass
-    def device_name(self):
-        print('my device here')
 # =================================
 class t2:
     def f(self,t):
@@ -17,23 +12,38 @@ class t2:
 
 if __name__=='__main__':
 
-    mydevice = device()
+    _ = mydevice()
 
-    _ = system_logs(mydevice)
+    # snooze time in units of seconds can be use to check how much time
+    # your code has been running and to check point your code
+    snooze_time = 2  
 
+    # singleton object
+    _ = system_logs(mydevice,snooze_time)
+
+    # print out information of machine etc
     system_logs.print_start_logs()
 
     t = t2()
- 
-    t.f(1)
-    time.sleep(1)
 
-    y = torch.randn([10000,10000],requires_grad=True)
-    w = torch.sum(y*y)
-    w.backward()
+    for i in range(7):
 
-    t.f(2)
+        t.f(1)
+        time.sleep(1)
+       
+        # check if it is time to checkpoint your code or not
+        if system_logs.check_alarm():
+            print('alarm sound, make snooze again')
+            print('we can use this alarm clock to decide if we want to checkpoint our code')
+       
+        y = mydevice.load(torch.randn([10000,10000],requires_grad=True))
+        w = torch.sum(y*y)
+        w.backward()
+       
+        t.f(2)
 
-    system_logs.print_end_logs()   
+        # print information of current running status
+        system_logs.print_current_logs()   
+        print('---------------------------------------')
 
 
