66cfd56d12751937a70ef70dbc5a6785c5351dc1
hwee kuan <hweekuan@yahoo.com>
2025-09-05 14:30:50 +0800

    auto generate config

---
+++ config_generator/config_generator.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+for lr in 1e-2 1e-3
+do
+  for batch in 32 64
+  do
+    for nepoch in 100 200
+    do
+      dirname="lr$lr-b$batch-n$nepoch"
+      mkdir $dirname
+      cd $dirname
+      ln -s ../dataset.pt .
+      cat ../config.tmpl | sed s/@lr@/$lr/g\
+                         | sed s/@batch@/$batch/g\
+                         | sed s/@nepoch@/$nepoch/g\
+                         > config.txt
+      cd ../
+    done
+  done
+done
